3e7c274fd57beea3b6427317fb601118b1404557
CLVM2004 <62641690+CLVM2004@users.noreply.github.com>
2022-04-19 17:56:36 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,15 +6,8 @@ sleep 3
 git clone "https://github.com/CLVM2004/Wrapper-Offline-For-MacOS.git"
 echo "Making all shell files executable..."
 cd Wrapper-Offline
-chmod +x start_wrapper.sh
-chmod +x update_wrapper.sh
-chmod +x import.sh
-cd utilities
-chmod +x open_http-server.sh
-chmod +x open_nodejs.sh
-chmod +x open_chromium.sh
-chmod +x audio_import.sh
-chmod +x photo_import.sh
+chmod +x ./make_executable.command
+./make_executable.command
 echo "Finished! (hopefully)"
 read -p "Press Return or Enter to exit."
 break
